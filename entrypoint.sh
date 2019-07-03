d9718ecf14e8374f73dd8e61eb25fc3c50eb927f
Neg <serg.zorg@gmail.com>
2019-07-03 17:49:18 +0300

    ekamboss support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-source /etc/profile.d/rvm.sh
-
-rvm use 2.5.3 --default
-bundle install -j 4 --without production development
-
-sed 's/peer/trust/' -i /etc/postgresql/10/main/pg_hba.conf
-
-cp -v config/database.yml.sample config/database.yml 
-cp -v config/secrets.sample.yml config/secrets.yml
-
-service postgresql restart 
-sleep 10
-
 bash -c "$*"
 
